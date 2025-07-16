a0ba333f0aabf2e742f304781e0b9b13b8c982e9
DanmaroXceed <daniel_martinez1103@outlook.com>
2025-07-16 11:19:15 -0600

    fix - trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        if(config('app.env') === 'production') {
             $middleware->trustProxies(at: '*');
-        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
